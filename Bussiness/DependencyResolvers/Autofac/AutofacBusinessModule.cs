2f0296ee60ffdbee0cc48f760f0bc1c893b19c48
cicekh <haydarcicekci@hotmail.com>
2021-03-25 10:51:05 +0300

    before Autofac implementation

---
+++ Bussiness/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using Bussiness.Abstract;
+using Bussiness.Concreate;
+using DataAccess.Abstract;
+using DataAccess.Concreate.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bussiness.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<CarManager>().As<ICarService>().SingleInstance();
+            builder.RegisterType<EfCarDal>().As<ICarDal>().SingleInstance();
+        }
+    }
+}
---
+++ Core/Utilities/Interceptors/AspectInterceptorSelector.cs
@@ -0,0 +1,23 @@
+﻿using Castle.DynamicProxy;
+using System;
+using System.Reflection;
+using System.Linq;
+
+namespace Core.Utilities.Interceptors
+{
+    public class AspectInterceptorSelector : IInterceptorSelector
+    {
+        public IInterceptor[] SelectInterceptors(Type type, MethodInfo method, IInterceptor[] interceptors)
+        {
+            var classAttributes = type.GetCustomAttributes<MethodInterceptionBaseAttribute>
+                (true).ToList();
+            var methodAttributes = type.GetMethod(method.Name)
+                .GetCustomAttributes<MethodInterceptionBaseAttribute>(true);
+            classAttributes.AddRange(methodAttributes);
+            //classAttributes.Add(new ExceptionLogAspect(typeof(FileLogger)));
+            //üstteki tüm işlemlerde loglamayı sağlar ama şu anda loglama altyapısı mevcut değil. Default olarak loglama yapmak istersen, Programcı logalama yaptı mı yapmadı mı düşünmeye gerek yok.
+            return classAttributes.OrderBy(x => x.Priority).ToArray();
+        }
+    }
+}
+ 
